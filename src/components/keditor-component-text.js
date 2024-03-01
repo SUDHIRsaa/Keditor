6985fa4793ceca8f615f63386700f33bc694eae4
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-03-01 21:06:18 +0530

    ohkk

--- src/components/keditor-component-text.js
+++ src/components/keditor-component-text.js
@@ -1,34 +1,49 @@
-import '../styles/keditor-component-text.less';
-
+import '../styles/SunedItor.css';
 import KEditor from 'keditor';
 import SunEditor from 'suneditor';
-import plugins from 'suneditor/src/plugins'
+import plugins from 'suneditor/src/plugins';
+
+
+// CKEDITOR.disableAutoInline = true;
+
+// CKEDITOR.dom.element.prototype.scrollIntoView = () => { return; };
+// CKEDITOR.dom.selection.prototype.scrollIntoView = () => { return; };
+// CKEDITOR.dom.range.prototype.scrollIntoView = () => { return; };
+
+
 
 
 KEditor.components['text'] = {
     options: {
-        plugins:[plugins.font],
-        plugins:[plugins.fontSize],
-        plugins:[plugins.formatBlock],
+        plugins: [
+            plugins.font,
+            plugins.fontSize,
+            plugins.formatBlock,
+            plugins.fontColor,
+            plugins.hiliteColor,
+            plugins.lineHeight,
+            plugins.link,
+            plugins.align,
+            plugins.list,
+            
+        ],
         buttonList: [
             [
-            
-            'font', 'fontSize', 'formatBlock',
-            'paragraphStyle', 'blockquote',
-            'bold', 'underline', 'italic', 'strike', 'subscript', 'superscript',
-            'fontColor', 'hiliteColor', 'textStyle',
-             'outdent', 'indent', 
-             'fullScreen', 'showBlocks', 'codeView',
-             'preview', 'print', 'save', 'template'
-     
+                'undo', 'redo',
+                'font', 'fontSize', 'formatBlock', 'lineHeight',
+                'fontColor', 'hiliteColor',
+                'bold', 'underline', 'italic', 'strike', 'removeFormat',
+                'list',
+                'outdent', 'indent', 'align',
+                'link',
+             
+                'save', 'template'
             ],
         ],
-        
-        mode: 'inline',
+        mode: 'classic',
         width: '100%',
         height: 'auto',
-        title:'editor',
-        
+        title: 'editor',
     },
 
     init: function (contentArea, container, component, keditor) {
@@ -53,10 +68,47 @@ KEditor.components['text'] = {
         });
 
         let editor = SunEditor.create(componentContent[0], self.options);
-       
-        
-      
+        // editor.on('instanceReady', function () {
+        //     $('#cke_' + componentContent.attr('id')).appendTo(keditor.wrapper);
+
+        //     if (typeof options.onComponentReady === 'function') {
+        //         options.onComponentReady.call(contentArea, component, editor);
+        //     }
+        // });
+
+        // editor.on('key', function (event) {
+        //     const isCtrl = event.data.domEvent.$.ctrlKey;
+        //     if ((isCtrl && event.data.domEvent.$.keyCode === 86) || event.data.domEvent.$.keyCode === 13) {
+        //         console.log('Dont scroll!!')
+        //         keditor.iframeBody.scrollTop($(editor.element.$).offset().top);
+        //         // componentContent.css('height', 200);
+        //         // keditor.iframeBody.css('overflow', 'visible');
+        //         setTimeout(() => {
+        //             // event.cancel();
+        //             // console.log(window);
+        //             // keditor.iframeBody.css('overflow', '');
+        //             // keditor.iframeBody.scrollTop($(editor.element.$).offset().top);
+        //         }, 10);
+        //     }
+        // }, editor);
     },
 
-  
+    // getContent: function (component, keditor) {
+    //     let componentContent = component.find('.keditor-component-content');
+    //     let id = componentContent.attr('id');
+    //     let editor = CKEDITOR.instances[id];
+    //     if (editor) {
+    //         return editor.getData();
+    //     } else {
+    //         return componentContent.html();
+    //     }
+    // },
+
+    // destroy: function (component, keditor) {
+    //     let id = component.find('.keditor-component-content').attr('id');
+    //     CKEDITOR.instances[id] && CKEDITOR.instances[id].destroy();
+    // },
 };
+    
+   
+    
